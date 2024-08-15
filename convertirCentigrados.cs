d78f5f79ed32f06be6dcf4d46d650eadde7ba62c
JoAVG <alfredopooo02@gmail.com>
2024-08-14 21:28:14 -0600

    add convertirCentigrados.cs y actualizacion en main
    Archivo nuevo con codigo para convertir los centigrados a kelvin y fahrenheit

---
+++ convertirCentigrados.cs
@@ -0,0 +1,18 @@
+﻿class convertirCentigrados
+{
+    private int convertirAKelvin(int Centigrados)
+    {
+        return Centigrados + 273;
+    }
+
+    private int convertirAFahrenheit(int Centigrados)
+    {
+        return Centigrados * 18 / 10 + 32;
+    }
+
+    public void getCentigradosConvertidos(int Centigrados)
+    {
+        Console.WriteLine("Kelvin = " + convertirAKelvin(Centigrados));
+        Console.WriteLine("Fahrenheit = " + convertirAFahrenheit(Centigrados));
+    }
+}
